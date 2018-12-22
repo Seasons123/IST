5fcc37104e482bec562df699707791048d3464ce
seasons123 <peichunyansd@gmail.com>
2018-12-22 10:47:48 +0800

    1222

--- js/public.js
+++ js/public.js
@@ -1,23 +1,32 @@
-var dataMode = 1; //0为使用本地模拟json数据，1为使用服务器数据
-
-//本地服务器 http://127.0.0.1:8081
-//内网服务器 http://10.15.1.34:8081
-var serverUrl = "http://10.15.1.34:8081";
-var functionCollection = {
-    "KpiConfig":  ['../data/target.json', "/df-pe/api/KpiConfig?"],
-    "QueryNextKpi":['../data/nextKpi.json', '/df-pe/api/Kpi?'],
-    "TaskKpi": ['../data/saveTaskKpiResponse.json', '/df-pe/api/TaskKpi?']
-};
-
-
-var formUrl = {};
-var initFormUrl = function() {
-    for(var key in functionCollection){
-        if(dataMode == 0){
-            formUrl[key] = functionCollection[key][dataMode];
-        }else if(dataMode == 1){
-            formUrl[key] = serverUrl + functionCollection[key][dataMode];
+//走网关用端口8081，直接走后台服务用端口8000
+var sever = "1";
+var formUrl = {} ;
+switch (sever){
+    case "0":
+        formUrl = {
+            "KpiConfig": '../data/target.json',
+            "QueryNextKpi": '../data/nextKpi.json',
+            "TaskKpi": '../data/saveTaskKpiResponse.json'
+        };
+        break ;
+    case  "1":
+        formUrl = {
+            "KpiConfig": 'http://10.10.65.43:8081/df-peeval/api/KpiConfig?',
+            "QueryNextKpi": 'http://10.10.65.43:8081/df-peeval/api/Kpi?',
+            "TaskKpi": 'http://10.10.65.43:8081/df-peeval/api/TaskKpi?'
+        };
+        break;
+    case  "2":
+        formUrl = {
+            "KpiConfig": ' http://10.15.1.34:8081/df-peeval/api/KpiConfig?',
+            "QueryNextKpi": 'http://10.15.1.34:8081/df-peeval/api/Kpi?',
+            "TaskKpi": 'http://10.15.1.34:8081/df-peeval/api/TaskKpi?'
+        };
+        break;
+    case  "3":
+        formUrl = {
+            "KpiConfig": 'http://127.0.0.1:8081/df-peeval/api/KpiConfig?',
+            "QueryNextKpi": 'http://127.0.0.1:8081/df-peeval/api/Kpi?',
+            "TaskKpi": 'http://127.0.0.1:8081/df-peeval/api/TaskKpi?'
         }
-    }
-};
-initFormUrl();
+}
